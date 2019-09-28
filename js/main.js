c8d911253ce1de3587ae58a9115ac46e9dbc7de2
raymag <prayk11@outlook.com>
2019-09-27 22:58:33 -0300

    navbar

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let toggleMenu = () => {
+    let navLink = document.querySelector('#nav-links');
+    navLink.classList.toggle('menu-active');
+    let burguerLines = document.querySelectorAll('.burger-line');
+    for( let i = 0; i<burguerLines.length; i++ ){
+        burguerLines[i].classList.toggle('menu-fuschia');
+    }
+}
+
+
+
+let navMenu = document.querySelector('#nav-menu');
+navMenu.addEventListener('click', toggleMenu);

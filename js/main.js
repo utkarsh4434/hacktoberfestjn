772101b4674f262d1b662a254a747d2cff40df0f
raymag <prayk11@outlook.com>
2019-09-28 19:12:57 -0300

    menu burguer update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let toggleMenu = () => {
     let navLink = document.querySelector('#nav-links');
     navLink.classList.toggle('menu-active');
+    document.querySelector('#nav-menu').classList.toggle('menu-active');
     let burguerLines = document.querySelectorAll('.burger-line');
     for( let i = 0; i<burguerLines.length; i++ ){
         burguerLines[i].classList.toggle('menu-fuschia');

11f8d72853879755193b7fe947a0d6b1877fea14
raymag <prayk11@outlook.com>
2019-09-29 20:14:12 -0300

    navbar close improved

--- js/main.js
+++ js/main.js
@@ -8,7 +8,39 @@ let toggleMenu = () => {
     }
 }
 
-
-
 let navMenu = document.querySelector('#nav-menu');
 navMenu.addEventListener('click', toggleMenu);
+
+let documentClick = (evt) => {
+    let navLink = document.querySelector('#nav-links');
+    if( navLink.classList.contains("menu-active") ){
+        let burguerLines = document.querySelectorAll('.burger-line');
+        let outside = true;
+
+        for(let i=0; i<burguerLines.length;i++){
+            if(evt.target == burguerLines[i]){
+                outside = false;
+            }
+        }
+        for(let i=0;i<navLink.children.length;i++){
+            if(evt.target == navLink.children[i] || evt.target == navLink.children[i].firstChild){
+                outside = false;
+            }
+        }
+
+        if( evt.target == navLink || evt.target == navMenu ){
+            outside = false;
+        }
+
+        if(outside){
+            navLink.classList.toggle('menu-active');
+            document.querySelector('#nav-menu').classList.toggle('menu-active');
+            for( let i = 0; i<burguerLines.length; i++ ){
+                burguerLines[i].classList.toggle('menu-fuschia');
+            }
+        }
+        
+    }
+}
+
+document.addEventListener("click", documentClick);

991e26958898b2ea3a66b93afe75736ce22495bd
raymag <prayk11@outlook.com>
2019-09-29 20:27:46 -0300

    navbar js functions refactored

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let toggleMenu = () => {
+toggleMenu = () => {
     let navLink = document.querySelector('#nav-links');
     navLink.classList.toggle('menu-active');
     document.querySelector('#nav-menu').classList.toggle('menu-active');
@@ -7,40 +7,35 @@ let toggleMenu = () => {
         burguerLines[i].classList.toggle('menu-fuschia');
     }
 }
-
-let navMenu = document.querySelector('#nav-menu');
-navMenu.addEventListener('click', toggleMenu);
-
-let documentClick = (evt) => {
+isClickOutsideNavbar  = (evt) => {
+    let outsideClick = true;    
+    let burguerLines = document.querySelectorAll('.burger-line');
+    let navLink = document.querySelector('#nav-links');
+    for(let i=0; i<burguerLines.length;i++){
+        if(evt.target == burguerLines[i]){
+            outsideClick = false;
+        }
+    }
+    for(let i=0;i<navLink.children.length;i++){
+        if(evt.target == navLink.children[i] || evt.target == navLink.children[i].firstChild){
+            outsideClick = false;
+        }
+    }
+    if( evt.target == navLink || evt.target == navMenu ){
+        outsideClick = false;
+    }
+    return outsideClick;
+}
+documentClick = (evt) => {
     let navLink = document.querySelector('#nav-links');
     if( navLink.classList.contains("menu-active") ){
-        let burguerLines = document.querySelectorAll('.burger-line');
-        let outside = true;
-
-        for(let i=0; i<burguerLines.length;i++){
-            if(evt.target == burguerLines[i]){
-                outside = false;
-            }
+        outsideClick = isClickOutsideNavbar(evt);
+        if(outsideClick){
+            toggleMenu();
         }
-        for(let i=0;i<navLink.children.length;i++){
-            if(evt.target == navLink.children[i] || evt.target == navLink.children[i].firstChild){
-                outside = false;
-            }
-        }
-
-        if( evt.target == navLink || evt.target == navMenu ){
-            outside = false;
-        }
-
-        if(outside){
-            navLink.classList.toggle('menu-active');
-            document.querySelector('#nav-menu').classList.toggle('menu-active');
-            for( let i = 0; i<burguerLines.length; i++ ){
-                burguerLines[i].classList.toggle('menu-fuschia');
-            }
-        }
-        
     }
 }
 
+let navMenu = document.querySelector('#nav-menu');
+navMenu.addEventListener('click', toggleMenu);
 document.addEventListener("click", documentClick);

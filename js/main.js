c65ad8c7987a833820e0eb0425bd1ddede16b70b
raymag <prayk11@outlook.com>
2019-09-30 19:59:27 -0300

    update

--- js/main.js
+++ js/main.js
@@ -3,34 +3,34 @@ toggleMenu = () => {
     navLink.classList.toggle('menu-active');
     document.querySelector('#nav-menu').classList.toggle('menu-active');
     let burguerLines = document.querySelectorAll('.burger-line');
-    for( let i = 0; i<burguerLines.length; i++ ){
+    for (let i = 0; i < burguerLines.length; i++) {
         burguerLines[i].classList.toggle('menu-fuschia');
     }
 }
-isClickOutsideNavbar  = (evt) => {
-    let outsideClick = true;    
+isClickOutsideNavbar = (evt) => {
+    let outsideClick = true;
     let burguerLines = document.querySelectorAll('.burger-line');
     let navLink = document.querySelector('#nav-links');
-    for(let i=0; i<burguerLines.length;i++){
-        if(evt.target == burguerLines[i]){
+    for (let i = 0; i < burguerLines.length; i++) {
+        if (evt.target == burguerLines[i]) {
             outsideClick = false;
         }
     }
-    for(let i=0;i<navLink.children.length;i++){
-        if(evt.target == navLink.children[i] || evt.target == navLink.children[i].firstChild){
+    for (let i = 0; i < navLink.children.length; i++) {
+        if (evt.target == navLink.children[i] || evt.target == navLink.children[i].firstChild) {
             outsideClick = false;
         }
     }
-    if( evt.target == navLink || evt.target == navMenu ){
+    if (evt.target == navLink || evt.target == navMenu) {
         outsideClick = false;
     }
     return outsideClick;
 }
 documentClick = (evt) => {
     let navLink = document.querySelector('#nav-links');
-    if( navLink.classList.contains("menu-active") ){
+    if (navLink.classList.contains("menu-active")) {
         outsideClick = isClickOutsideNavbar(evt);
-        if(outsideClick){
+        if (outsideClick) {
             toggleMenu();
         }
     }
